fcea16a960bdbf7028f7588183f7a2be4d66afd8
Allison Mackey <a.mackey693@gmail.com>
2020-07-26 22:30:58 -0700

    add foreign key for posts

---
+++ db/migrate/20200727052948_add_foreign_key_for_posts.rb
@@ -0,0 +1,5 @@
+class AddForeignKeyForPosts < ActiveRecord::Migration[5.2]
+  def change
+    add_foreign_key :posts, :users
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_27_045416) do
+ActiveRecord::Schema.define(version: 2020_07_27_052948) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,4 +53,5 @@ ActiveRecord::Schema.define(version: 2020_07_27_045416) do
   end
 
   add_foreign_key "comments", "posts"
+  add_foreign_key "posts", "users"
 end
