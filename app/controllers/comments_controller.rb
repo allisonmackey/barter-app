ecd4fde2276ebe3dc3677ca06caf3b26c4a25d6c
Allison Mackey <a.mackey693@gmail.com>
2020-07-26 22:24:26 -0700

    add initial controllers for posts and comments

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,31 @@
+class CommentsController < ApplicationController
+
+  def index
+    # Code for listing all comments goes here.
+  end
+
+  def new
+    # Code for new comment form goes here.
+  end
+
+  def create
+    # Code for creating a new comment goes here.
+  end
+
+  def edit
+    # Code for edit comment form goes here.
+  end
+
+  def show
+    # Code for showing a single comment goes here.
+  end
+
+  def update
+    # Code for updating an comment goes here.
+  end
+
+  def destroy
+    # Code for deleting an comment goes here.
+  end
+
+end
---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,31 @@
+class PostsController < ApplicationController
+
+  def index
+    # Code for listing all posts goes here.
+  end
+
+  def new
+    # Code for new post form goes here.
+  end
+
+  def create
+    # Code for creating a new post goes here.
+  end
+
+  def edit
+    # Code for edit post form goes here.
+  end
+
+  def show
+    # Code for showing a single post goes here.
+  end
+
+  def update
+    # Code for updating an post goes here.
+  end
+
+  def destroy
+    # Code for deleting an post goes here.
+  end
+
+end
