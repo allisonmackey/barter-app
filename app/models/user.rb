acb307899563342b979dfbcf0e97ba058ba6e059
Allison Mackey <a.mackey693@gmail.com>
2020-07-27 12:01:22 -0700

    add active storage to user model for profile photo capability

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_one_attached :profile_photo
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :posts, dependent: :destroy
