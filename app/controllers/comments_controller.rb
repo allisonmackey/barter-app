9af9dab50f85d8b818e2cb0d36098aa0025fe100
Allison Mackey <a.mackey693@gmail.com>
2020-07-26 22:52:16 -0700

    add basic methods to comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,31 +1,54 @@
-class CommentsController < ApplicationController
+class CommentsController < ApplicationController 
 
-  def index
-    # Code for listing all comments goes here.
+  def index 
+    render :home
   end
 
   def new
-    # Code for new comment form goes here.
+    @post = Post.find(params[:post_id])
+    @comment= Comment.new
   end
 
   def create
-    # Code for creating a new comment goes here.
+    # will need to refactor to @post.comments.new(comment_params)
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
   end
 
   def edit
-    # Code for edit comment form goes here.
+   @post = Post.find(params[:post_id])
+   @comment = Comment.find(params[:id])
   end
 
   def show
-    # Code for showing a single comment goes here.
+   @post = Post.find(params[:post_id])
+   @comment = Comment.find(params[:id])
   end
 
   def update
-    # Code for updating an comment goes here.
+    @post = Post.find(params[:post_id)]
+    @comment = Comment.find(params[:id])
+    if @comment.update(comment_params)
+      redirect_to post_path(@comment.post)
+    else 
+      render :edit
+    end
   end
 
   def destroy
-    # Code for deleting an comment goes here.
+    @comment= Comment.find(params[:id])
+    @comment.destroy
+    redirect_to post_path(@comment.post)
+    render :index
   end
 
+  private 
+    def comment_params
+      params.require(:comment).permit(:author, :body, :offer, :offer_type)
+    end
 end
